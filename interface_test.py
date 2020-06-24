0f11e808a8f46b33bb66f59521df6aa71a81ea52
MICCHL <64470680+MICCHL@users.noreply.github.com>
2020-06-24 18:46:43 +0200

    Add files via upload

--- interface_test.py
+++ interface_test.py
@@ -6,11 +6,10 @@ import interface
 class TestInterface(unittest.TestCase):
 
     def setUp(self):
-        self.Maschine = interface.Maschine()
+        self.maschine = interface.Maschine()
 
     def test_throwing(self):
-        result = self.Maschine.sum_coins(1.0)
-        self.assertEqual(result, 1.0)
+        self.assertEqual(self.maschine.throw_coins(1.0), 1.0)
 
 
 if __name__ == '__main__':
