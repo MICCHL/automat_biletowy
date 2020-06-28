59783bc7cbf0fe8ea48d8d3bdeaa390db2e79f88
MICCHL <64470680+MICCHL@users.noreply.github.com>
2020-06-28 23:27:29 +0200

    Add files via upload

---
+++ constant.py
@@ -0,0 +1,13 @@
+class Constants:
+    MAIN_FRAME_TITLE = "Automat biletowy MPK"
+    PAYMENT_TITLE = "Portfel"
+    BUTTON_WIDTH = 25
+    BUTTON_HEIGHT = 5
+    COLOUR = '#9F9FDF'
+    TEN = 10
+    ELEVEN = 11
+    NOMINAL = [50.0, 20.0, 10.0, 5.0, 2.0, 1.0, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01]
+    AMOUNT = [3, 5, 4, 0, 12, 0, 14, 0, 4, 0, 55, 100]
+    TICKET_NAMES = ["Ulgowy 20 min", "Normalny 20 min", "Ulgowy 50 min ", "Normalny 50 min ",
+                    "Ulgowy 90 min", "Normalny 90 min "]
+    TICKET_PRICES = [1.70, 3.40, 2.30, 4.60, 3, 6]
