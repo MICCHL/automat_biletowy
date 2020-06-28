03d44dddc67c2bca35af6e5dcde5f6c6066ccc24
MICCHL <64470680+MICCHL@users.noreply.github.com>
2020-06-28 23:27:43 +0200

    Add files via upload

--- Give_Back.py
+++ Give_Back.py
@@ -1,7 +1,15 @@
 import tkinter as tk
 
+from colour import Colour
 
-class Give_Back(tk.Frame):
+
+class GiveBackFrame(tk.Frame):
+    COINS_LIST_HEIGHT = 20
+    COINS_LIST_WIDTH = 25
+    TAKE_MONEY_BTN_HEIGHT = 3
+    TAKE_MONEY_BTN_WIDTH = 28
+    TAKE_MONEY = "Zabierz"
+
     def __init__(self, master, bc, text):
 
         super().__init__(master)
@@ -11,36 +19,39 @@ class Give_Back(tk.Frame):
         self.create_widgets(master)
         self.center()
 
-    def create_widgets(self, x):
+    def create_widgets(self, master):
 
         top_frame = tk.Frame(self)
         top_frame.pack(fill=tk.Y)
         low_frame = tk.Frame(self)
         low_frame.pack(side=tk.BOTTOM, fill=tk.Y)
 
-        self.coins_list = tk.Text(top_frame, width=25, height=20, bg="white")
-        self.coins_list.pack()
-        self.write_coins()
+        coins_list = tk.Text(top_frame, width=GiveBackFrame.COINS_LIST_WIDTH,
+                             height=GiveBackFrame.COINS_LIST_HEIGHT, bg=Colour.WHITE)
+        coins_list.pack()
+        self.write_coins(coins_list)
 
-        self.key1 = tk.Button(low_frame, text="Zabierz", height=3, width=28,
-                           background="red", command=lambda: self.back(x))
-        self.key1.pack()
+        take_button = tk.Button(low_frame, text=GiveBackFrame.TAKE_MONEY,
+                                height=GiveBackFrame.TAKE_MONEY_BTN_HEIGHT,
+                                width=GiveBackFrame.TAKE_MONEY_BTN_WIDTH,
+                                background=Colour.RED, command=lambda: self.back(master))
+        take_button.pack()
 
-    def write_coins(self):
+    def write_coins(self, coins_list):
 
-        lista = '\n'.join([f'{self.typ} :'] + [str(c) for c in self.coins])
+        coins = '\n'.join([f'{self.typ} :'] + [str(c) for c in self.coins])
 
-        self.coins_list.insert(0.0, lista)
+        coins_list.insert(0.0, coins)
 
-    def back(self, x):
-
-        x.quit()
-        x.destroy()
+    @staticmethod
+    def back(master):
+        master.quit()
+        master.destroy()
 
     def center(self):
         self.master.update_idletasks()
         width = self.master.winfo_width()
         height = self.master.winfo_height()
-        x = (self.master.winfo_screenwidth() // 2) - (width // 2)
-        y = (self.master.winfo_screenheight() // 2) - (height // 2)
-        self.master.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+        center_x = (self.master.winfo_screenwidth() // 2) - (width // 2)
+        center_y = (self.master.winfo_screenheight() // 2) - (height // 2)
+        self.master.geometry('{}x{}+{}+{}'.format(width, height, center_x, center_y))
